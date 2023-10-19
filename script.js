c28b5d0b11d5072de0325253a14060d79d494034
Carlos (Evelynn) Borges <97672739+CarlosBorges6158@users.noreply.github.com>
2023-10-18 21:58:52 -0300

    Adicionando as funções do Vue JS a um arquivo .js.

---
+++ script.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#usuarios',
+    data: {
+        users: []
+    },
+    mounted: function () {
+        this.loadUsers();
+    },
+    methods: {
+        loadUsers: function () {
+
+            fetch('https://reqres.in/api/users?per_page=10')
+                .then(response => response.json())
+                .then(data => {
+                    this.users = data.data;
+                })
+                .catch(error => {
+                    console.error('Erro ao carregar os usuários:', error);
+                });
+        }
+    }
+});
